1874db3d24fe8426bc23bc6cd4e53f84341387bd
Yosof Badr <yosof.badr@siemens.com>
2019-10-16 13:01:41 +0100

    Instal ejs and set up general app.js structure

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ var express = require("express"),
     mongoose = require("mongoose"),
     bodyParser = require("body-parser");
 
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.listen("3000", function(){ 
+  console.log("Blog is listening on port 3000");
+});
