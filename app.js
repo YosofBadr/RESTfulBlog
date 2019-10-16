acd736ddb282f22c0288f3bb04bc33f5536fafc7
Yosof Badr <yosof.badr@siemens.com>
2019-10-16 13:05:25 +0100

    Configure mongoose database, schema and model

--- app.js
+++ app.js
@@ -3,9 +3,21 @@ var express = require("express"),
     mongoose = require("mongoose"),
     bodyParser = require("body-parser");
 
+mongoose.connect("mongodb://localhost:27017/restfulblog", { useNewUrlParser: true, useUnifiedTopology: true }); 
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+var blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
 
 
 app.listen("3000", function(){ 
